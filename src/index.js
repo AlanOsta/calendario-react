3304853fe41e397b086056a6c7e46d5af7411813
Alan Osta <alanosta@gmail.com>
2021-02-11 16:15:28 -0300

    CSS de Botones

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import calendarioReducer from './reducers/calendarioReducer';
 import thunk from "redux-thunk"
 
-const store = createStore(calendarioReducer,compose (applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+//const store = createStore(calendarioReducer,compose (applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+// Para que Firefox no de error
+//const store = createStore( calendarioReducer, compose( applyMiddleware(thunk), window.navigator.userAgent.includes('Chrome') ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose, ), )
+
+//Para el build y no tire problemas de seguridad
+const store = createStore(calendarioReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
